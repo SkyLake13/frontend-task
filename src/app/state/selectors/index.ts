903027b33dd8eface2f44a9c29b0421afc71528c
Abhishek Kumar <skylake1302@gmail.com>
2021-09-24 16:35:35 +0200

    Minor refactoring

--- src/app/state/selectors/index.ts
+++ src/app/state/selectors/index.ts
@@ -1,12 +1,1 @@
-import { createSelector } from '@ngrx/store';
-import { AppState } from '../app.state';
-
-import { CountriesState } from '../reducers/countries.reducer';
-
-
-export const selectCountriesState = (state: AppState) => state.countries;
-
-export const selectCountries = createSelector(
-  selectCountriesState,
-  (state: CountriesState) => state.countries,
-);
+export * from './countries.selector';
