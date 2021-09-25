2417f9952b4ff714277ece6d1d0a25d8dfcb79a4
Abhishek Kumar <skylake1302@gmail.com>
2021-09-25 23:43:14 +0200

    UI Refinements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ],
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(
+    private readonly location: Location,
+    public  readonly route: ActivatedRoute
+  ) { }
+
+  public goBack() {
+    this.location.back();
+  }
+}
