f1742d639d04d876a280f85184572ee0efc05c1f
Abhishek Kumar <skylake1302@gmail.com>
2021-09-27 18:33:00 +0200

    Added few more unit tests

--- src/app/country-list/components/country-list-presentation/presentation.component.spec.ts
+++ src/app/country-list/components/country-list-presentation/presentation.component.spec.ts
@@ -1,16 +1,44 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { By } from '@angular/platform-browser';
+import { CountryPipe } from '@country-list/country-pipe/country.pipe';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { CountryListPresentationComponent } from './presentation.component';
 
 describe('CountryListPresentationComponent', () => {
+  const initialState = {
+    countries: {
+      countries: [
+        {
+          cca3: 'DEU',
+          name: {
+            common: 'Germany',
+            official: 'Germany'
+          },
+          region: '',
+          subregion: '',
+          capital: [''],
+          area: 89780,
+          borders: [''],
+          flags: ['']
+        }
+      ]
+    }
+  };
+
   let component: CountryListPresentationComponent;
   let fixture: ComponentFixture<CountryListPresentationComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CountryListPresentationComponent ],
-      schemas: [NO_ERRORS_SCHEMA]
+      imports: [MatTableModule],
+      declarations: [ CountryListPresentationComponent, CountryPipe ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        provideMockStore({ initialState: initialState }),
+      ],
     })
     .compileComponents();
   });
@@ -22,5 +50,27 @@ describe('CountryListPresentationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render input', () => {
+    const data = [
+      {
+        name: 'germany',
+        officialName : 'germany',
+        capital: ['Berline'],
+        region: 'europe',
+        borders: ['FRA'],
+        area: 98798,
+        flag: 'jhkjh',
+        cca3: 'DEU',
+      }
+    ]
+    component.dataSource = new MatTableDataSource(data);
+
+    fixture.detectChanges();
+
+    const nameColumns = fixture.debugElement.queryAll(By.css('td.mat-column-name'));
+    expect(nameColumns.length).toEqual(1);
+    expect(nameColumns[0].nativeElement.textContent).toEqual('germany');
   });
 });
