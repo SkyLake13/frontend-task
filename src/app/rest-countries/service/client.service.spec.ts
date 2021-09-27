20eef6037826b355acbca54aa80f5e096e64ea5f
Abhishek Kumar <skylake1302@gmail.com>
2021-09-27 16:02:39 +0200

    Unit tests added for API Client service

--- src/app/rest-countries/service/client.service.spec.ts
+++ src/app/rest-countries/service/client.service.spec.ts
@@ -1,11 +1,14 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { Subscription } from 'rxjs';
 
 import { API_BASE_URL } from '../injection-tokens';
 import { ClientService } from './client.service';
 
-describe('ClientService', () => {
+fdescribe('ClientService', () => {
   let service: ClientService;
+  let httpTestingController: HttpTestingController;
+  let subscription: Subscription;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -18,10 +21,72 @@ describe('ClientService', () => {
         },
       ]
     });
+  });
+
+  beforeEach(() => {
     service = TestBed.inject(ClientService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+    subscription?.unsubscribe();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get All Countries', (done) => {
+    const response = [
+      {
+          cca3: 'DEU',
+          name: {
+            common: 'Germany'
+          },
+          capital: ['Berlin'],
+          region: 'europe',
+          subregion: '',
+          borders: [],
+          area: 8798,
+          flags: []
+      }
+    ];
+
+    subscription = service.getAllCountries$().subscribe((countries) => {
+      expect(countries).toBeDefined();
+      expect(countries[0].cca3).toEqual('DEU');
+
+      done();
+    });
+
+    const req = httpTestingController.expectOne('https://restcountries.com/v3/all');
+    req.flush(response);
+  });
+
+  it('should get Country by code', (done) => {
+    const response = [{
+          cca3: 'DEU',
+          name: {
+            common: 'Germany'
+          },
+          capital: ['Berlin'],
+          region: 'europe',
+          subregion: '',
+          borders: [],
+          area: 8798,
+          flags: []
+      }];
+
+    subscription = service.getCountryByCode$('DEU').subscribe((country) => {
+      console.log(country);
+      expect(country).toBeDefined();
+      expect(country.cca3).toEqual('DEU');
+
+      done();
+    });
+
+    const req = httpTestingController.expectOne('https://restcountries.com/v3/alpha/DEU');
+    req.flush(response);
   });
 });
